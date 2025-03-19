d17803cd054d39e15ee0861ebf64213ceeeb8086
Matthieu Charpentier <macharpe@local.mac>
2025-03-19 10:47:40 +0100

    test signing

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,6 +6,7 @@ terraform {
       name = "terraform-cloudflare-macharpe"
     }
   }
+  required_version = ">= 1.11.0"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
